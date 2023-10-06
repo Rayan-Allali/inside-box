d7f8aa2290907851540b621218bca9d625cbf686
Rayan-Allali <108684980+Rayan-Allali@users.noreply.github.com>
2023-10-07 00:49:33 +0100

    training ressources trainer view

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 import SignIn from "@/components/SignIn";
 import "./globals.css";
-
+import {TrainingRessources} from "@/components/teacher/ressouces/TrainingRessource"
 export default function Home() {
   return (
     <main>
-      <SignIn />
+      <TrainingRessources />
     </main>
   );
 }
