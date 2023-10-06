9d43805d6f575d526fb953bbb23f396e628cf2d4
Ryaaad <devil.fine116@gmail.com>
2023-10-06 16:03:43 +0100

    module importing err

--- src/app/AdminLayout.tsx
+++ src/app/AdminLayout.tsx
@@ -1,7 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import SideNavbar from '@/components/Shared/SideNavbar'
+import SideNavbar from '@/components/Shared/Sidenavbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
