4217d734e7856ff7dbc8f70233a9e5fd7242a37a
Ayyoub Kasmi <ayyoubkasmidz@gmail.com>
2023-10-06 15:27:49 +0200

    EditTrainer form done

--- src/app/[username]/page.tsx
+++ src/app/[username]/page.tsx
@@ -1,12 +1,16 @@
 "use client"
 import AddTrainer from "@/components/Admin/AddTrainer"
+import EditTrainer from "@/components/Admin/EditTrainer"
 import { useParams } from "next/navigation"
 
 const UserHome = () => {
     const { username } = useParams()
 
     return (
-        <AddTrainer/>
+        <>
+            {/* <AddTrainer/> */}
+            {/* <EditTrainer/> */}
+        </>
     )
 }
 
