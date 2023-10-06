7ede6ac6b17b336eade59650ed02fedaf7d7f76c
Ayyoub Kasmi <ayyoubkasmidz@gmail.com>
2023-10-06 16:41:28 +0200

    AddTrainee and EditTrainee added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,27 @@
 import Image from "next/image";
 import SignIn from "@/components/SignIn";
-import SideNavbar from "@/components/Shared/Sidenavbar";
+import SideNavbar from "@/components/Shared/SideNavbar";
 import Trainings from "@/components/Admin/Trainings";
+import AddTrainer from "@/components/Admin/Trainers/AddTrainer";
+import EditTrainer from "@/components/Admin/Trainers/EditTrainer";
+import AddTrainee from "@/components/Admin/Trainees/AddTrainee";
+import EditTrainee from "@/components/Admin/Trainees/EditTrainee";
 
 export default function Home() {
+  // route detection
+
+  // components selection
+
+  // conditional rendering
+
   return (
     <main>
-      <SignIn />
+      {/* <SignIn /> */}
+      {/* <Trainings/> */}
+      {/* <AddTrainer/> */}
+      {/* <EditTrainer/> */}
+      {/* <AddTrainee/> */}
+      <EditTrainee/>
     </main>
   );
 }
