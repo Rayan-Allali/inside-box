d51d496a2abef9589ca94919450dac2e919716bc
Ryaaad <devil.fine116@gmail.com>
2023-10-07 10:32:17 +0100

    routes

---
+++ src/app/StudentKids/assignment/page.tsx
@@ -0,0 +1,11 @@
+import KidAssignement from "@/components/Student/kids/KidAssignment";
+
+export default function Home() {
+
+    return (
+      <main  className="p-20 py-14" >
+      <KidAssignement></KidAssignement>
+      </main>
+    );
+  }
+
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,10 @@
 import SignIn from "@/components/SignIn";
 import "./globals.css";
-import { TeacherTraining } from "@/components/teacher/Training";
-import { TeacherGeneral } from "@/components/teacher/myTrainings";
-import TrainingRessources from "@/components/teacher/ressouces/TrainingRessource";
-import TalkingBox from "@/components/Student/kids/meetings/TalkingBox";
-import OnlineClass from "@/components/Student/kids/meetings/OnlineClass";
-import AdultStudentGeneral from "@/components/Student/AdultStudentGeneral/AdultStudentRessources";
-import AssignmentContainer from "@/components/Student/AssignmentContainer";
-// import AdultStudentGeneral from "@/components/Student/AdultStudentGeneral/AdultStudentGeneral"; li khs fiha lfo9
+
 export default function Home() {
   return (
     <main>
- <SignIn />  
+      <SignIn />
     </main>
   );
 }
